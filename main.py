de8388f4f0a39687b328f3cda67097c561cc8212
Skylar Wurster <swwurster@gmail.com>
2020-11-05 10:49:55 -0500

    Update gitignore

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import torch
 import numpy as np
 import os
-import elevation
+import matplotlib.pyplot as plt
 
-a = elevation.clip(bounds=(12.35, 41.8, 12.65, 42))
-print(a)
+# data from http://www.viewfinderpanoramas.org/dem3.html
+# http://viewfinderpanoramas.org/Coverage%20map%20viewfinderpanoramas_org3.htm
+byteorder = "big"
 
-elevation.clean()
+for foldername in os.listdir("./RawData"):
+    for filename in os.listdir(os.path.join("./RawData", foldername)):
+        print(filename.split(".")[0])
+        a = np.fromfile(os.path.join('./RawData', foldername, filename), dtype=np.byte)
+
+        res = int((a.shape[0]/2)**0.5)
+        b = np.zeros([res, res])
+
+        for i in range(int(a.shape[0]/2)):
+            row = int(i / res)
+            col = i % res
+            b[row, col] = 256*int.from_bytes(a[i*2], byteorder) + int.from_bytes(a[(i*2)+1], byteorder)
+        
+        np.save(os.path.join("./NumpyRawData", foldername + "_" + filename.split(".")[0]+".npy"), b)
