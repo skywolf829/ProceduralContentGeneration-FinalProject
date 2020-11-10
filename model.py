fe61029d49f53542e4580f880b757090b2eccd50
Skylar Wurster <swwurster@gmail.com>
2020-11-10 17:56:37 -0500

    Update model.py

--- model.py
+++ model.py
@@ -54,6 +54,8 @@ def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv2d') != -1:
         m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('Linear') != -1:
+        m.weight.data.normal_(0.0, 0.02)
     elif classname.find('Norm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
